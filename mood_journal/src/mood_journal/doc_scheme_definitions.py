a8c8be81410775e357e722c012d1f254f026d33c
evoosa <eevvoossaa@gmail.com>
2020-05-04 20:03:37 +0300

    created diagrams for the DB, needs a HEAVY check

--- mood_journal/src/mood_journal/doc_scheme_definitions.py
+++ mood_journal/src/mood_journal/doc_scheme_definitions.py
@@ -13,35 +13,34 @@ connect(db=MOOD_JOURNAL_DB_NAME, host=DB_HOST, port=DB_PORT)
 
 class Forms(Document):
     """ Definition for the forms collection """
-    fill_date = DateField(required=True, unique=True, primary_key=True)
+    fill_date = DateField(required=True, primary_key=True)
     submission_timestamp = DateTimeField(required=True)
     saddest = IntField(required=True)
     happiest = IntField(required=True)
     happy_reasons = ListField(ReferenceField(HappyReasons, reverse_delete_rule='DENY'), required=True)
-    # it is stored as DBRef automatcally
+        # it is stored as DBRef automatcally !
     sad_reasons = ListField(ReferenceField(SadReasons, reverse_delete_rule='DENY'), required=True)
-    # TODO - use this
-    # https://docs.mongoengine.org/guide/defining-documents.html#one-to-many-with-listfields
+        # TODO - use this
+        # https://docs.mongoengine.org/guide/defining-documents.html#one-to-many-with-listfields
     detox_progress = IntField(required=True)
-    detox_pitfalls = ListField(ReferenceField(DetoxPitchfallReasons, reverse_delete_rule='DENY'), required=True)
+    detox_pitfalls = ListField(ReferenceField(DetoxFailReasons, reverse_delete_rule='DENY'), required=True)
     to_keep = StringField(required=True)
     to_improve = StringField(required=True)
 
 
-class Reasons(Document):
-    """ a class for other Reason classes to derive from """
-    reason = StringField(required=True, unique=True, primary_key=True)
-    rating_frequency = MapField(IntField(required=True, default=0), required=True)
-    pass
+class HappyReasons(Document):
+    """ a class for the Reasons to be Happy today """
+    happy_reason = StringField(required=True, primary_key=True)
+    happy_rates_freq = MapField(IntField(required=True, default=0), required=True)
 
 
-class HappyReasons(Reasons):
-    super()
+class SadReasons(Document):
+    """ a class for the Reasons to be Sad today """
+    sad_reason = StringField(required=True, primary_key=True)
+    sad_rates_freq = MapField(IntField(required=True, default=0), required=True)
 
 
-class SadReasons(Reasons):
-    super()
-
-
-class DetoxPitchfallReasons(Reasons):
-    super()
+class DetoxFailReasons(Document):
+    """ a class for the Reasons i Failed the Dopamine Detox today """
+    detox_fail_reason = StringField(required=True, primary_key=True)
+    detox_rates_freq = MapField(IntField(required=True, default=0), required=True)
