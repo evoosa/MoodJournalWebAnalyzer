046ec1cd14d185a84c76b203594f960121baa90d
evoosa <eevvoossaa@gmail.com>
2020-05-09 13:44:40 +0300

    added helper funcs

--- mood_journal/src/mood_journal/consts.py
+++ mood_journal/src/mood_journal/consts.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-import os.path
+import os
 
 DB_HOST = '192.168.223.128'
 DB_PORT = 27017
@@ -12,7 +12,12 @@ DETOX_FAIL_REASONS_COLLECTION_NAME = 'detox_fail_reasons'
 
 MOOD_JOURNAL_FORM_ID = '1UWZwqU-RE8WvqWc5NOHNhhWGzbHFOtkDymwwcD938ww'
 FORM_RESULTS_URL = 'https://docs.google.com/spreadsheets/d/{form_id}/export?exportFormat=csv'.format(form_id=MOOD_JOURNAL_FORM_ID)
-RESULTS_DIR = 'C:\\Users\\evoosa\\Desktop\\Projects\\Python\\MoodJournalWebAnalyzer\\FoolingAround' # TODO - fit it for linux!
+
+WIN_RESULTS_DIR = 'C:\\Users\\evoosa\\Desktop\\Projects\\Python\\MoodJournalWebAnalyzer\\FoolingAround'
+LIN_RESULTS_DIR = '/tmp/' # FIXME to a legit path thnks
+
+RESULTS_DIR = WIN_RESULTS_DIR if os.name == 'nt' else LIN_RESULTS_DIR
 
 RESULTS_CSV_FILENAME = 'form_results-{curr_datetime}.csv'.format(curr_datetime=datetime.now().strftime("%m-%d-%Y_%H-%M"))
 RESULTS_CSV_PATH = os.path.join(RESULTS_DIR, RESULTS_CSV_FILENAME)
+
--- mood_journal/src/mood_journal/utils.py
+++ mood_journal/src/mood_journal/utils.py
@@ -1,19 +1,23 @@
 from mongoengine import connect
 import urllib.request
 import shutil
-from .consts import FORM_RESULTS_URL, RESULTS_CSV_PATH
-
-from .consts import MOOD_JOURNAL_DB_NAME, DB_HOST, DB_PORT
+import pandas as pd
+from mood_journal.consts import FORM_RESULTS_URL, RESULTS_CSV_PATH, MOOD_JOURNAL_DB_NAME, DB_HOST, DB_PORT
 
 
 def get_results_data_from_google_docs():
-    """ copy the up to date results CSV file from Google Docs locally """
+    """ Copy the up to date results CSV file from Google Docs to a local path """
     with urllib.request.urlopen(FORM_RESULTS_URL) as response, open(RESULTS_CSV_PATH, 'wb') as results_csv:
         shutil.copyfileobj(response, results_csv)
 
 
-def convert_csv_to_rows_dict():
-    pass
+def convert_results_csv_to_rows_dict() -> dict:
+    """
+    Convert the results CSV to a dict containing it's rows
+    :return: dict containing the results CSV's rows
+    """
+    data_frame = pd.read_csv(RESULTS_CSV_PATH, header=0)
+    return data_frame.to_dict(orient='records')[1:]
 
 
 def connect_to_db():
